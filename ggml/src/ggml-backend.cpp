46de2fda5af570661697154660c7ef32f9feeeba
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2025-12-24 21:44:10 +0900

    fix wasm build
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- ggml/src/ggml-backend.cpp
+++ ggml/src/ggml-backend.cpp
@@ -660,9 +660,10 @@ static bool ggml_is_view_op(enum ggml_op op) {
 
 // scheduler
 
-#ifndef GGML_SCHED_MAX_BACKENDS
-#define GGML_SCHED_MAX_BACKENDS 16
-#endif
+#define GGML_SCHED_MAX_BACKENDS 128
+// #ifndef GGML_SCHED_MAX_BACKENDS
+// #define GGML_SCHED_MAX_BACKENDS 16
+// #endif
 
 #ifndef GGML_SCHED_MAX_SPLIT_INPUTS
 #define GGML_SCHED_MAX_SPLIT_INPUTS 30
